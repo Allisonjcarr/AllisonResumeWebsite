a247a6957adbffdf8d7841abe1ea8507a1824667
Allison Carr <allisonjcarr7@gmail.com>
2024-10-20 10:33:46 -0400

    db stuff

--- allison_resume/db.py
+++ allison_resume/db.py
@@ -6,6 +6,9 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./website.db")
+# Replace 'postgres://' with 'postgresql://'
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
